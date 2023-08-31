bbb8d4cae219de1c57f853b1762ae1228338f816
CodewithRupam <117833981+CodewithRupam@users.noreply.github.com>
2023-08-31 12:15:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ window.addEventListener("load", () => {
 })
 var typed = new Typed('#id', {
   strings: ['<i>Web developer</i>', '<i>UI/ UX designer</i>', '<i>Video edittor</i>'],
-  typeSpeed: 25,
+  typeSpeed: 200,
 });
 
 var typed = new Typed('#text', {
   strings: ['<strong>Rupam</strong>'],
-  typeSpeed: 50,
+  typeSpeed: 200,
 });
 let space = document.getElementById("open");
 let message = document.getElementById("message");
@@ -50,10 +50,10 @@ var ctx = document.getElementById('UI').getContext('2d');
 var myPieChart = new Chart(ctx, {
   type: 'pie',
   data: {
-    labels: ['Colour theory', 'Layout', 'Responsive design', 'Typography'],
+    labels: ['Layout', 'Responsive design', 'Typography'],
     datasets: [{
-      data: [20, 30, 30, 20],
-      backgroundColor: ['pink', 'green', 'lightblue', 'violet'],
+      data: [30, 30, 20],
+      backgroundColor: ['green', 'lightblue', 'violet'],
     }],
   },
   options: {
